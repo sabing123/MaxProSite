59d2046e63bc4828cd7784609cb09f7a36bb2e62
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2021-02-25 09:14:38 +0545

    contact merged

--- MaxproApp/views.py
+++ MaxproApp/views.py
@@ -22,3 +22,23 @@ def aboutus(request):
     ab3 = Aboutus.objects.filter(about_category='About Classes')
     params = {'ab1': ab1, 'ab2': ab2, 'ab3': ab3}
     return render(request, 'about.html', params)
+<<<<<<< Updated upstream
+=======
+
+def contact(request):
+    if request.method == 'POST':
+        f = ContactForm(request.POST)
+        if f.is_valid():
+            name = f.cleaned_data['name']
+            sender = f.cleaned_data['email']
+            subject = "You have a new Feedback from {}:{}".format(name, sender)
+            message = "Subject: {}\n\nMessage: {}".format(f.cleaned_data['subject'], f.cleaned_data['message'])
+            mail_admins(subject, message)
+            f.save()
+            messages.add_message(request, messages.INFO, 'Feedback Submitted.')
+            return redirect('contact')
+    else:
+        f = ContactForm()
+    return render(request, 'contact.html', {'form': f})
+
+>>>>>>> Stashed changes
