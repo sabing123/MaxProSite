a048539fa7eead0f1411f18d8a8fc0ee46324123
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2021-04-24 13:57:05 +0545

    contact page completed

--- MaxproApp/models.py
+++ MaxproApp/models.py
@@ -10,20 +10,7 @@ class Aboutus(models.Model):
 
     def __str__(self):
         return self.about_category
-
-
-class Contact(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
-    subject = models.CharField(max_length=200)
-    message = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural = "Contact"
-
-    def __str__(self):
-        return self.name + "-" +  self.email
+        
 
 
 class CourseOffered(models.Model):
