77733acc9dd7bff697f37c12f1bad06bbc048629
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2021-05-02 09:59:28 +0545

    update tables

--- MaxproApp/admin.py
+++ MaxproApp/admin.py
@@ -1,21 +1,12 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-from .models import Aboutus, Contact, CourseOffered, Gallery, PremiumCourses
-=======
-from .models import Aboutus, CourseOffered, Gallery, StudentRegister
->>>>>>> Rupesh-Branch
+from .models import Aboutus, CourseOffered, Gallery, PremiumCourses, StudentRegister
 
 # Register your models here.
 admin.site.register(Aboutus)
-admin.site.register(Contact)
 admin.site.register(CourseOffered)
 admin.site.register(Gallery)
-<<<<<<< HEAD
 admin.site.register(PremiumCourses)
-=======
 admin.site.register(StudentRegister)
->>>>>>> Rupesh-Branch
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'subject','date',)
